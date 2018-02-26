a26299fbdb97134ba158a2f25ae0e101da9547d3
田口良則 <tagutagu@qb3.so-net.ne.jp>
2018-02-26 14:23:37 +0900

    新規登録

---
+++ SalesManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalesManagement.Startup))]
+namespace SalesManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
